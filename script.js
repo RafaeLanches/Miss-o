cf6d066b2ebfc1bb2d5431f153f1560fe812e465
RafaeLanches <145361883+RafaeLanches@users.noreply.github.com>
2025-07-01 11:36:31 -0300

    .

---
+++ script.js
@@ -0,0 +1,23 @@
+const caixaPrincipal = document.querySelector('caixa-principal');
+const caixaPerguntas = document.querySelector('caixa-perguntas');
+const caixaAlternativas = document.querySelector('caixa-Alternativas');
+const caixaResultado = document.querySelector('caixa-resultado');
+const textoResltado = document.querySelector('texto-resultado');
+
+const perguntas= [
+    {
+        enunciado:Assim que saiu da escola
+        alternativas:  [
+            "isso é assutador!"
+            "Isso é maravilhoso"
+        ]
+    },
+
+        {
+        enunciado:Assim que saiu da escola
+        alternativas:  [
+            "isso é assutador!"
+            "Isso é maravilhoso"
+        ]
+    },
+]
